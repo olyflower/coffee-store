2445f6d64cd1e3fd46aa5e3f44379f0fa4888b82
olyflower <2013.olga.yudina@gmail.com>
2023-09-17 11:09:24 +0300

    Added modal window, buy_product.js

--- views/parts/home/catalog.php
+++ views/parts/home/catalog.php
@@ -15,7 +15,14 @@ $products = array_chunk($products, 3);
         <?php foreach($products as $row): ?>
             <div class="row">
                 <?php foreach ($row as $product): ?>
-                    <div class="col-12 col-md-4 catalog-item">
+                    <div class="col-12 col-md-4 catalog-item"
+                         data-bs-toggle="modal"
+                         data-bs-target="#buyProduct"
+                         data-id="<?= $product['id'] ?>"
+                         data-qty="<?= $product['quantity'] ?>"
+                         data-name="<?= $product['name'] ?>"
+                         data-price="<?= $product['price'] ?>"
+                     >
                         <div class="catalog-item-body">
                             <div class="d-flex justify-content-between catalog-item-title">
                                 <h4><?= $product['name'] ?></h4>
